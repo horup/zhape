3ca7ef84851c78980d2d1e66c53d012272f515b5
horup <sah@sah.dk>
2017-08-17 12:12:32 +0200

    added THREE.JS support

--- src/view/perspective/index.tsx
+++ src/view/perspective/index.tsx
@@ -1,8 +1,16 @@
 import * as React from 'react';
 import {State} from './../../model';
+import * as THREE from 'three';
+
 export default class Perspective extends React.Component<{enable:boolean, sharedState:State}, any>
 {
+    canvas:HTMLCanvasElement;
+    width:number;
+    height:number;
     sharedState:State;
+    scene:THREE.Scene = new THREE.Scene(); 
+    renderer:THREE.WebGLRenderer;
+    camera:THREE.Camera;
 
     constructor(props:any)
     {
@@ -10,11 +18,46 @@ export default class Perspective extends React.Component<{enable:boolean, shared
         this.sharedState = props.sharedState;
     }
 
+    componentDidMount()
+    {
+        this.renderer = new THREE.WebGLRenderer({canvas:this.canvas});
+        this.renderer.setClearColor( 0xff0000, 1 );
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 10000);
+        this.animate();
+    }
+
+    lambda = ()=>this.animate();
+    private animate()
+    {
+        let time = new Date().getTime();
+        this.resize();
+
+        this.renderer.render(this.scene, this.camera);
+
+        requestAnimationFrame(this.lambda);
+    }
+
+    resize()
+    {
+        if (this.width != window.innerWidth || this.height != window.innerHeight)
+        {
+            let cam = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 10000);
+            this.camera.copy(cam);
+            this.renderer.setSize(window.innerWidth, window.innerHeight);
+            this.camera.up.set(0,0,1);
+            this.camera.position.set(0, 0, 0.5);
+            
+            this.camera.lookAt(new THREE.Vector3(32, -32, 0.5));
+            this.width = window.innerWidth;
+            this.height = window.innerHeight;
+        }
+    }
+
     render()
     {
         return (
             <div>
-                {this.sharedState.map.edges.length}
+                <canvas ref={(ref)=>this.canvas = ref}/>
             </div>
         );
     }
